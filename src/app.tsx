b6f6a6495e24174ec3b9834f9a61ea48f5bdec63
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-12 19:11:48 -0700

    refactor event listeners

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,17 @@
 import { BlurFilter } from 'pixi.js'
 import { Stage, Container, Sprite, Text } from '@pixi/react'
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import styles from './app.module.scss'
+
+type Container = HTMLDivElement | null
 
 export function App() {
   const blurFilter = useMemo(() => new BlurFilter(4), [])
 
-  const [container, setContainer] = useState<HTMLDivElement | null>(null)
+  const [container, setContainer] = useState<Container>(null)
   const rect = container?.getBoundingClientRect()
+  useEventListeners(container)
 
   return (
     <div className={styles.app} ref={setContainer}>
@@ -39,4 +42,35 @@ export function App() {
       )}
     </div>
   )
+}
+
+function useEventListeners(container: Container) {
+  useEffect(() => {
+    if (!container) return
+
+    const ac = new AbortController()
+    const { signal } = ac
+
+    container.addEventListener(
+      'pointermove',
+      () => {
+        console.log('pointermove')
+      },
+      { signal },
+    )
+
+    container.addEventListener(
+      'wheel',
+      (e) => {
+        console.log('wheel')
+
+        e.preventDefault()
+      },
+      { signal, passive: false },
+    )
+
+    return () => {
+      ac.abort()
+    }
+  }, [container])
 }
