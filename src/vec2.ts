0ffa31a7065bb2d30fc4257d26dd8c99927b66c9
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-13 08:58:49 -0700

    fix vec2 constructor

--- src/vec2.ts
+++ src/vec2.ts
@@ -4,9 +4,9 @@ export class Vec2 {
   readonly x: number
   readonly y: number
 
-  constructor(x: number = 0, y: number = 0) {
+  constructor(x: number = 0, y?: number) {
     this.x = x
-    this.y = y
+    this.y = y ?? x
   }
 
   add(v: Vec2): Vec2 {
