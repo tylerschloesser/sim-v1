4936ee4a72118a81ef6a89fe73486466d701c415
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-13 10:44:16 -0700

    refactor app hooks

---
+++ src/app.hooks.ts
@@ -0,0 +1,63 @@
+import { useEffect } from 'react'
+import invariant from 'tiny-invariant'
+import { pointer$, viewport$, wheel$ } from './state.js'
+import { hackPointerEvent } from './util.js'
+import { Vec2 } from './vec2.js'
+
+export function useEventListeners(container: HTMLDivElement | null) {
+  useEffect(() => {
+    if (!container) return
+
+    const ac = new AbortController()
+    const { signal } = ac
+
+    container.addEventListener(
+      'pointermove',
+      (e) => {
+        pointer$.next(hackPointerEvent(e))
+      },
+      { signal },
+    )
+
+    container.addEventListener(
+      'pointerleave',
+      (e) => {
+        pointer$.next(hackPointerEvent(e))
+      },
+      { signal },
+    )
+
+    container.addEventListener(
+      'wheel',
+      (e) => {
+        wheel$.next(e)
+        e.preventDefault()
+      },
+      { signal, passive: false },
+    )
+
+    return () => {
+      ac.abort()
+    }
+  }, [container])
+}
+
+export function useResizeObserver(container: HTMLDivElement | null) {
+  useEffect(() => {
+    if (!container) return
+
+    const ro = new ResizeObserver((entries) => {
+      invariant(entries.length === 1)
+      const entry = entries.at(0)
+      invariant(entry)
+      viewport$.next(
+        new Vec2(entry.contentRect.width, entry.contentRect.height),
+      )
+    })
+
+    ro.observe(container)
+    return () => {
+      ro.disconnect()
+    }
+  }, [container])
+}
--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,10 @@
 import { Stage } from '@pixi/react'
 import { Subscribe } from '@react-rxjs/core'
-import { useEffect, useState } from 'react'
-import invariant from 'tiny-invariant'
+import { useState } from 'react'
 import styles from './app.module.scss'
 import { GridContainer } from './grid-container.js'
 import { HoverContainer } from './hover-container.js'
-import { pointer$, viewport$, wheel$ } from './state.js'
-import { hackPointerEvent } from './util.js'
-import { Vec2 } from './vec2.js'
+import { useEventListeners, useResizeObserver } from './app.hooks.js'
 
 export function App() {
   const [container, setContainer] = useState<HTMLDivElement | null>(null)
@@ -34,62 +31,4 @@ export function App() {
       )}
     </div>
   )
-}
-
-function useEventListeners(container: HTMLDivElement | null) {
-  useEffect(() => {
-    if (!container) return
-
-    const ac = new AbortController()
-    const { signal } = ac
-
-    container.addEventListener(
-      'pointermove',
-      (e) => {
-        pointer$.next(hackPointerEvent(e))
-      },
-      { signal },
-    )
-
-    container.addEventListener(
-      'pointerleave',
-      (e) => {
-        pointer$.next(hackPointerEvent(e))
-      },
-      { signal },
-    )
-
-    container.addEventListener(
-      'wheel',
-      (e) => {
-        wheel$.next(e)
-        e.preventDefault()
-      },
-      { signal, passive: false },
-    )
-
-    return () => {
-      ac.abort()
-    }
-  }, [container])
-}
-
-function useResizeObserver(container: HTMLDivElement | null) {
-  useEffect(() => {
-    if (!container) return
-
-    const ro = new ResizeObserver((entries) => {
-      invariant(entries.length === 1)
-      const entry = entries.at(0)
-      invariant(entry)
-      viewport$.next(
-        new Vec2(entry.contentRect.width, entry.contentRect.height),
-      )
-    })
-
-    ro.observe(container)
-    return () => {
-      ro.disconnect()
-    }
-  }, [container])
 }
