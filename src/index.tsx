898dd714426f14771a4d65ae86c3037a6415d040
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-12 18:32:46 -0700

    pixi

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,31 @@
-console.log('hi')
+import { BlurFilter } from 'pixi.js'
+import { Stage, Container, Sprite, Text } from '@pixi/react'
+import { useMemo } from 'react'
+
+export const MyComponent = () => {
+  const blurFilter = useMemo(() => new BlurFilter(4), [])
+
+  return (
+    <Stage>
+      <Sprite
+        image="https://pixijs.io/pixi-react/img/bunny.png"
+        x={400}
+        y={270}
+        anchor={{ x: 0.5, y: 0.5 }}
+      />
+
+      <Container x={400} y={330}>
+        <Text
+          text="Hello World"
+          anchor={{ x: 0.5, y: 0.5 }}
+          filters={[blurFilter]}
+        />
+      </Container>
+    </Stage>
+  )
+}
+
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('app')
+const root = createRoot(container!)
+root.render(<MyComponent />)
