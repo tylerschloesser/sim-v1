4b1a73721cdda710643c4b061ab33c74b6266a2f
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-14 08:45:36 -0700

    update grid

--- src/grid-container.tsx
+++ src/grid-container.tsx
@@ -3,8 +3,9 @@ import * as PIXI from 'pixi.js'
 import { useCallback } from 'react'
 import { useCamera, useViewport } from './state.js'
 import { getCellSize } from './util.js'
+import { CHUNK_SIZE } from './const.js'
 
-export function GridContainer() {
+function CellGrid() {
   const camera = useCamera()
   const viewport = useViewport()
 
@@ -38,5 +39,51 @@ export function GridContainer() {
     [viewport, camera],
   )
 
-  return <Graphics draw={draw} />
+  return <Graphics draw={draw} alpha={camera.zoom} />
+}
+
+function ChunkGrid() {
+  const camera = useCamera()
+  const viewport = useViewport()
+
+  const draw = useCallback(
+    (g: PIXI.Graphics) => {
+      const cellSize = getCellSize(camera.zoom)
+
+      g.clear()
+
+      g.lineStyle({
+        width: 1 / cellSize,
+        color: '0x111',
+      })
+
+      let topLeft = camera.position.sub(viewport.div(2).div(cellSize))
+      let bottomRight = topLeft.add(viewport.div(cellSize))
+
+      topLeft = topLeft.div(CHUNK_SIZE).floor().mul(CHUNK_SIZE)
+      bottomRight = bottomRight.div(CHUNK_SIZE).ceil().mul(CHUNK_SIZE)
+
+      for (let x = topLeft.x; x <= bottomRight.x; x += CHUNK_SIZE) {
+        g.moveTo(x, topLeft.y)
+        g.lineTo(x, bottomRight.y)
+      }
+
+      for (let y = topLeft.y; y <= bottomRight.y; y += CHUNK_SIZE) {
+        g.moveTo(topLeft.x, y)
+        g.lineTo(bottomRight.x, y)
+      }
+    },
+    [viewport, camera],
+  )
+
+  return <Graphics draw={draw} alpha={1 - camera.zoom} />
+}
+
+export function GridContainer() {
+  return (
+    <>
+      <CellGrid />
+      <ChunkGrid />
+    </>
+  )
 }
