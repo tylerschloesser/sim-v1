cd4e8abdb6eaa41a422409d363258f82965a59b7
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-13 18:07:26 -0700

    reorder hover

--- src/app.tsx
+++ src/app.tsx
@@ -24,8 +24,8 @@ export function App() {
           }}
         >
           <Subscribe>
-            <HoverContainer />
             <GridContainer />
+            <HoverContainer />
           </Subscribe>
         </Stage>
       )}
