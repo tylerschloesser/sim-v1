3d6605b51469b15af268ce60f81fc185e1df2894
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-13 18:13:44 -0700

    add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import { App } from './app.js'
 import './index.scss'
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import invariant from 'tiny-invariant'
+
 const container = document.getElementById('root')
-const root = createRoot(container!)
-root.render(<App />)
+invariant(container)
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <>TODO handle errors</>,
+  },
+])
+
+createRoot(container).render(<RouterProvider router={router} />)
