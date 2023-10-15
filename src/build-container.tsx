081460274d8d6b7b2e21d48c9f11c841e88465f1
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-14 17:28:06 -0700

    add missing file

---
+++ src/build-container.tsx
@@ -0,0 +1,21 @@
+import { Graphics } from '@pixi/react'
+import * as PIXI from 'pixi.js'
+import { useCallback } from 'react'
+import { useBuild } from './state.js'
+
+export function BuildContainer() {
+  const build = useBuild()
+
+  const draw = useCallback(
+    (g: PIXI.Graphics) => {
+      g.clear()
+      g.beginFill('red')
+      g.drawRect(0, 0, 2, 2)
+    },
+    [build],
+  )
+
+  if (build === null) return null
+
+  return <Graphics draw={draw} />
+}
