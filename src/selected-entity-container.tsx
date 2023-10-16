5cfe1ee3d34559489e02b5b86102879df049efa1
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-16 11:41:32 -0700

    refactored hover

--- src/selected-entity-container.tsx
+++ src/selected-entity-container.tsx
@@ -1,11 +1,36 @@
+import { Sprite, useApp } from '@pixi/react'
 import * as PIXI from 'pixi.js'
+import { useEffect, useState } from 'react'
 import invariant from 'tiny-invariant'
 import { useEntities, useSelectedEntityIds } from './state.js'
 import { Entity } from './types.js'
-import { useCallback } from 'react'
-import { Graphics } from '@pixi/react'
+import { Vec2 } from './vec2.js'
+
+type Textures = Record<string, PIXI.Texture>
 
 export function SelectedEntityContainer() {
+  const app = useApp()
+  const [textures, setTextures] = useState<Textures | null>(null)
+
+  useEffect(() => {
+    const _textures: Textures = {}
+    const g = new PIXI.Graphics()
+    for (const size of [new Vec2(1), new Vec2(2)]) {
+      g.clear()
+      const lineWidth = 0.1
+      g.beginFill('transparent')
+      g.lineStyle(lineWidth, 'yellow')
+      g.drawRect(
+        lineWidth / 2,
+        lineWidth / 2,
+        size.x - lineWidth,
+        size.y - lineWidth,
+      )
+      _textures[`${size.x}.${size.y}`] = app.renderer.generateTexture(g)
+    }
+    setTextures(_textures)
+  }, [app])
+
   const selectedEntityIds = useSelectedEntityIds()
   const entities = useEntities()
 
@@ -18,22 +43,14 @@ export function SelectedEntityContainer() {
     })
   }
 
-  const draw = useCallback(
-    (g: PIXI.Graphics) => {
-      g.clear()
-      g.beginFill('transparent')
-      g.lineStyle(0.1, 'yellow')
-      for (const entity of selectedEntities) {
-        g.drawRect(
-          entity.position.x,
-          entity.position.y,
-          entity.size.x,
-          entity.size.y,
-        )
-      }
-    },
-    [selectedEntities],
-  )
+  if (!textures) return null
 
-  return <Graphics draw={draw} />
+  return selectedEntities.map((entity) => {
+    const texture = textures[`${entity.size.x}.${entity.size.y}`]
+    invariant(texture)
+
+    return (
+      <Sprite key={entity.id} texture={texture} position={entity.position} />
+    )
+  })
 }
