eeaf5991c6114bf49853da00ef2685ac4dab8290
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-18 16:50:11 -0700

    add house constructor

--- src/house-container.ts
+++ src/house-container.ts
@@ -2,5 +2,9 @@ import { EntityContainer } from './entity-container-v2.js'
 import { Entity } from './types.js'
 
 export class HouseContainer extends EntityContainer {
+  constructor() {
+    super()
+  }
+
   update(entity: Entity): void {}
 }
