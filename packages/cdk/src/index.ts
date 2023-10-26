33b7a5495970f67955e0f9ae8e37c8eb07b3e04f
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-26 14:04:12 -0700

    refactor certificate stack

--- packages/cdk/src/index.ts
+++ packages/cdk/src/index.ts
@@ -1,36 +1,10 @@
-import { App, Environment, Stack } from 'aws-cdk-lib'
-import {
-  Certificate,
-  CertificateValidation,
-} from 'aws-cdk-lib/aws-certificatemanager'
-import { HostedZone } from 'aws-cdk-lib/aws-route53'
-import { Construct } from 'constructs'
+import { App } from 'aws-cdk-lib'
 import { CdnStack } from './cdn-stack.js'
+import { CertificateStack } from './certificate-stack.js'
 import { DnsStack } from './dns-stack.js'
-import { CommonStackProps, Domain, Region } from './types.js'
+import { Domain, Region } from './types.js'
 
 const app = new App()
-
-interface CertificateStackProps extends CommonStackProps {
-  demoHostedZone: HostedZone
-  env: Omit<Required<Environment>, 'region'> & { region: Region.US_EAST_1 }
-}
-
-class CertificateStack extends Stack {
-  public readonly certificate: Certificate
-  constructor(
-    scope: Construct,
-    id: string,
-    { domain, demoHostedZone, ...props }: CertificateStackProps,
-  ) {
-    super(scope, id, props)
-
-    this.certificate = new Certificate(this, 'Certificate', {
-      domainName: domain.demo,
-      validation: CertificateValidation.fromDns(demoHostedZone),
-    })
-  }
-}
 
 const ACCOUNT_ID: string = '063257577013'
 
