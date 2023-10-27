210aa277568ff524d8421d5bb74c2a41aaab5b78
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-27 15:08:16 -0700

    show visible chunk count

--- packages/web/src/debug-overlay.tsx
+++ packages/web/src/debug-overlay.tsx
@@ -1,12 +1,15 @@
 import styles from './debug-overlay.module.scss'
 import { useAverageTickDuration } from './debug.js'
+import { useVisibleChunkIds } from './state.js'
 
 export function DebugOverlay() {
   const averageTickDuration = useAverageTickDuration()
+  const visibleChunkIds = useVisibleChunkIds()
 
   return (
     <div className={styles.container}>
-      Avg Tick: {averageTickDuration.toFixed(2)}ms
+      <div>Avg Tick: {averageTickDuration.toFixed(2)}ms</div>
+      <div>Visible Chunks: {visibleChunkIds.size}</div>
     </div>
   )
 }
